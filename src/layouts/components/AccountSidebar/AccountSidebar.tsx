a9529c70f13bf1e60cdcdf00d162d939bd3b2cde
Tran Hai Trieu <haitrieu2524@gmail.com>
2023-07-25 23:59:00 +0700

    Thực hiện chức năng logout ở account sidebar

--- src/layouts/components/AccountSidebar/AccountSidebar.tsx
+++ src/layouts/components/AccountSidebar/AccountSidebar.tsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames';
-import { Fragment } from 'react';
+import { Fragment, useContext } from 'react';
 import { NavLink } from 'react-router-dom';
+import { useMutation } from '@tanstack/react-query';
 
 import {
   DefaultUserIcon,
@@ -11,8 +12,24 @@ import {
   UserFillIcon
 } from 'src/components/Icons';
 import PATH from 'src/constants/path';
+import authApi from 'src/apis/auth.api';
+import { AppContext } from 'src/contexts/app.context';
 
 const AccountSidebar = () => {
+  const { setIsAuthenticated, setProfile } = useContext(AppContext);
+
+  const logoutMutation = useMutation({
+    mutationFn: authApi.logout,
+    onSuccess: () => {
+      setIsAuthenticated(false);
+      setProfile(null);
+    }
+  });
+
+  const logout = () => {
+    logoutMutation.mutate();
+  };
+
   return (
     <Fragment>
       <div className='p-4 border-b border-[#cfcfcf] flex items-center mb-[6px]'>
@@ -68,18 +85,10 @@ const AccountSidebar = () => {
           <EyeFillIcon className='w-[18px] h-[18px] mr-3' />
           <span>Sản phẩm đã xem</span>
         </NavLink>
-        <NavLink
-          to={PATH.HOME}
-          className={({ isActive }) =>
-            classNames('flex items-center py-3 px-[20px]', {
-              'text-primary font-semibold': isActive,
-              'hover:text-primary': !isActive
-            })
-          }
-        >
+        <button className={classNames('flex items-center w-full py-3 px-[20px] hover:text-primary')} onClick={logout}>
           <LogoutFillIcon className='w-[18px] h-[18px] mr-3' />
           <span>Đăng xuất</span>
-        </NavLink>
+        </button>
       </div>
     </Fragment>
   );
